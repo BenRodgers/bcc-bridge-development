b582675490b85ae9985efb033685c5f848c4b3a4
BenRodgers <benjaminarodgers@gmail.com>
2021-05-27 14:55:02 +1000

    Downloading File from Google Cloud Storage

--- survey_response_cleaning.R
+++ survey_response_cleaning.R
@@ -1,1 +1,18 @@
-#Script for reading survey response
+#Script for reading Google Cloud Surveys
+
+library(usethis)
+library('data.table')
+library(cloudml)
+library(readr)
+library(googleCloudStorageR)
+
+gcs_global_bucket("bcc-bridge-survey")
+proj <- 'practice-gcp-310913'
+buckets <- gcs_list_buckets(proj)
+bucket <- "bcc-bridge-survey"
+bucket_info <- gcs_get_bucket(bucket)
+objects <- gcs_list_objects()
+gcs_get_object(objects$name[[1]], saveToDisk = "bcc-bridge-survey-working.csv")
+
+
+
